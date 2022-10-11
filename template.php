765dff1af875ddb9a388b43bf0b179f2d286574f
yashzadafiya5 <104185063+yashzadafiya5@users.noreply.github.com>
2022-10-11 15:37:39 +0530

    newcode

--- template.php
+++ template.php
@@ -8,6 +8,55 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
+    <style>
+         div.introduction {
+            padding: 1em;
+            background: #f5f5f5;
+            box-shadow: 3px 3px 8px hsl(0, 0%, 70%);
+        }
+        
+        /* Card Based Layout - Base styles */
+        
+        p.site-title {
+          color: #000000;
+        }
+        
+        div.crd-content {
+          background: #f5f5f5;
+          padding: 0.5em;
+          margin-top: 1em;  
+          margin-bottom: 2em;
+          box-shadow: 3px 3px 8px hsl(0, 0%, 70%);
+        }
+        
+        div.crd-content h3 {
+          margin-bottom: 0.5em;
+          font-weight: normal;
+        }
+        
+        div.crd-content p {
+          font-size: 95%;
+        }
+        
+        img.spectrum1 {
+          width: 100%;
+          height: auto;
+        }
+        
+        div a, a:hover {
+          text-decoration: none;
+        }
+        
+        div a:hover {
+          opacity: 0.8;
+        }
+        #img
+        {
+            width: 100%;
+            height: 300px;
+        }
+        
+    </style>
    <?php 
     require_once("inc/css.php");
     ?>
@@ -20,35 +69,107 @@ require_once("inc/menu.php");
         <!-- Overlay Start -->
         <div class="overlay"></div>
         <!-- Overlay End -->
-        <!-- Page banner Section Start -->
-        <div class="section page-banner-section" style="background-image: url(theme/theme10/assets/images/page-banner.jpg);">
-            <div class="container">
-                <!-- Page banner Section Start -->
-                <div class="page-banner-content">
-                    <h2 class="page-title">Contact Us</h2>
+            <!-- Page banner Section Start -->
+            <div class="section page-banner-section" style="background-image: url(theme/theme10/assets/images/page-banner.jpg);">
+                <div class="container">
+                    <!-- Page banner Section Start -->
+                    <div class="page-banner-content">
+                        <h2 class="page-title">Bootstrap Code Snippets</h2>
 
-                    <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item active">Contact</li>
-                    </ul>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active">Snippets</li>
+                        </ul>
+                    </div>
+                    <!-- Page banner Section End -->
+
                 </div>
-                <!-- Page banner Section End -->
-
             </div>
-        </div>
         <!-- Page banner Section End -->
-        <!-- Contact Map Start -->
-        <div class="section section-padding-02">
-            <div class="container">
-                <!-- Contact Map Wrapper Start -->
-                <div class="contact-map-wrapper">
-                    <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=Mission%20District%2C%20San%20Francisco%2C%20CA%2C%20USA&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed"></iframe>
-                </div>
-                <!-- Contact Map Wrapper End -->
+ 
+        
 
+        <!-- main cards -->
+    <div class="container">
+    <div class="row">
+    
+    
+        <article class="col-md-4 col-lg-3" id='card'>
+            <div class="card-content crd-content">
+            <a class="spectrum-a" href="http://uj.ac.za"><img id="img" src="images/Untitled.png"  alt="meow">
+            <h4 class="spectrum-h2">Spectrum</h4>
+                <p>Visit our world-class analytical facility for South African scientists, Spectrum</p></a>
             </div>
-        </div>
-        <!-- Contact Map End -->
+            <!-- .card-content -->
+        </article>
+        <!-- .card -->
+        
+        <!-- .card -->
+        <article class="col-md-4 col-lg-3" id='card'>
+            <div class="card-content crd-content">
+            <a class="spectrum-a" href="http://uj.ac.za"><img id="img" src="images/Untitled.png"  alt="meow">
+            <h4 class="spectrum-h2">Spectrum</h4>
+                <p>Visit our world-class analytical facility for South African scientists, Spectrum</p></a>
+            </div>
+            <!-- .card-content -->
+        </article>
+        <!-- .card -->
+        
+        <!-- .card -->
+        <article class="col-md-4 col-lg-3" id='card'>
+            <div class="card-content crd-content">
+            <a class="spectrum-a" href="http://uj.ac.za"><img id="img" src="images/Untitled.png"  alt="meow">
+            <h4 class="spectrum-h2">Spectrum</h4>
+                <p>Visit our world-class analytical facility for South African scientists, Spectrum</p></a>
+            </div>
+            <!-- .card-content -->
+        </article>
+        <!-- .card -->
+        <!-- .card -->
+        <article class="col-md-4 col-lg-3" id='card'>
+            <div class="card-content crd-content">
+            <a class="spectrum-a" href="http://uj.ac.za"><img id="img" src="images/Untitled.png"  alt="meow">
+            <h4 class="spectrum-h2">Spectrum</h4>
+                <p>Visit our world-class analytical facility for South African scientists, Spectrum</p></a>
+            </div>
+            <!-- .card-content -->
+        </article>
+        <!-- .card -->
+        <!-- .card -->
+        <article class="col-md-4 col-lg-3" id='card'>
+            <div class="card-content crd-content">
+            <a class="spectrum-a" href="http://uj.ac.za"><img id="img" src="images/Untitled.png"  alt="meow">
+            <h4 class="spectrum-h2">Spectrum</h4>
+                <p>Visit our world-class analytical facility for South African scientists, Spectrum</p></a>
+            </div>
+            <!-- .card-content -->
+        </article>
+        <!-- .card -->
+        <!-- .card -->
+        <article class="col-md-4 col-lg-3" id='card'>
+            <div class="card-content crd-content">
+            <a class="spectrum-a" href="http://uj.ac.za"><img id="img" src="images/Untitled.png"  alt="meow">
+            <h4 class="spectrum-h2">Spectrum</h4>
+                <p>Visit our world-class analytical facility for South African scientists, Spectrum</p></a>
+            </div>
+            <!-- .card-content -->
+        </article>
+        <!-- .card -->
+    
+   
+    </div>
+    </div>
+    </div>
+        <!-- main cards -->
+
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script
+      src="https://kit.fontawesome.com/266b76cb57.js"
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
       <?php
 require_once("inc/footer.php");
 require_once("inc/script.php");
